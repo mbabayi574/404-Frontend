99fbe65bd3d5e63b613994fe9ef0d937730286ae
mbabayi574 <m.babayi574@gmail.com>
2022-03-19 16:21:29 +0330

    fixes

--- frontend404/src/Routes.tsx
+++ frontend404/src/Routes.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { Switch, Route, BrowserRouter as Router } from "react-router-dom";
 import Login from "./views/Login_SignUp/Login/login";
+import Signup from "./views/Login_SignUp/Signup_CompanyOwner/signup";
+
 import Dashboard from "./Pages/Dashboard";
 
 const Routes = () => {
@@ -9,7 +11,7 @@ const Routes = () => {
       <div>
         <Switch>
           <Route exact path="/" component={Login} />
-          <Route path="/signup_company" component={Signup_Company} />
+          <Route path="/signup" component={Signup} />
           <Route exact path="/home" component={Dashboard} />
         </Switch>
       </div>
