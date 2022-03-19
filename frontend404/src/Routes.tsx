917dc6fefb20fed805b7905188dc344dd379a010
mbabayi574 <m.babayi574@gmail.com>
2022-03-19 16:17:48 +0330

    routes fix

--- frontend404/src/Routes.tsx
+++ frontend404/src/Routes.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { Switch, Route, BrowserRouter as Router } from "react-router-dom";
 import Login from "./views/Login_SignUp/Login/login";
-import Signup_Company from "./views/Login_SignUp/Signup_CompanyOwner/signup_company";
 import Dashboard from "./Pages/Dashboard";
 
 const Routes = () => {
