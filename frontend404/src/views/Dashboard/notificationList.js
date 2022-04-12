09c9f996ad1f2b6b169add38afcd21565766ad86
Diyar Hamedi <diyarhamedi81@gmail.com>
2022-04-12 21:08:35 +0430

    Add iew all button to notification list

--- frontend404/src/views/Dashboard/notificationList.js
+++ frontend404/src/views/Dashboard/notificationList.js
@@ -2,15 +2,28 @@ import Paper from "@mui/material/Paper";
 import Stack from "@mui/material/Stack";
 import Item from "@mui/material/Stack";
 import Typography from "@mui/material/Typography";
+import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
 import Notification from './notification';
 
 const NotificationList = ({notifications}) => {
     return <Paper sx={{
         padding: 2
     }}>
-        <Typography variant="h5" component="h1">
-            Notifications
-        </Typography>
+        <Box style={{
+            display: "flex",
+            flexDirection: "row",
+            alignItems: "center",
+        }}>
+            <Typography variant="h5" component="h1">
+                Notifications
+            </Typography>
+            <Box marginLeft="auto">
+                <Button>
+                    View all
+                </Button>
+            </Box>
+        </Box>
         <Stack marginTop={1} spacing={0.5}>
         {
             notifications.map(({title, date}) => {
