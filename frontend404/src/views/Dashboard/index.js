8190369a06e10ff84859f6752be4d4c8241dac0d
Diyar Hamedi <diyarhamedi81@gmail.com>
2022-04-18 19:03:42 +0430

    Remove old app bar code

--- frontend404/src/views/Dashboard/index.js
+++ frontend404/src/views/Dashboard/index.js
@@ -77,23 +77,6 @@ const Dashboard = () => {
       }),
     }),
   );
-  
-  // const AppBar = styled(MuiAppBar, {
-  //   shouldForwardProp: (prop) => prop !== 'open',
-  // })(({ theme, open }) => ({
-  //   transition: theme.transitions.create(['margin', 'width'], {
-  //     easing: theme.transitions.easing.sharp,
-  //     duration: theme.transitions.duration.leavingScreen,
-  //   }),
-  //   ...(open && {
-  //     width: `calc(100% - ${drawerWidth}px)`,
-  //     marginLeft: `${drawerWidth}px`,
-  //     transition: theme.transitions.create(['margin', 'width'], {
-  //       easing: theme.transitions.easing.easeOut,
-  //       duration: theme.transitions.duration.enteringScreen,
-  //     }),
-  //   }),
-  // }));
 
   const DrawerHeader = styled('div')(({ theme }) => ({
     display: 'flex',
