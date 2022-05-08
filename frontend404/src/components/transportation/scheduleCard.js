14d65929614d8884a83d3df22213a88df93357ed
Diyar Hamedi <diyarhamedi81@gmail.com>
2022-05-08 09:47:13 +0430

    Add details of 'View Transportation Schedules' page

--- frontend404/src/components/transportation/scheduleCard.js
+++ frontend404/src/components/transportation/scheduleCard.js
@@ -1,15 +1,92 @@
 import Box from '@mui/material/Box';
 import Card from '@mui/material/Card';
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
+import Tooltip from '@mui/material/Tooltip';
+import Divider from '@mui/material/Divider';
+import Typography from '@mui/material/Typography';
+import EditIcon from '@mui/icons-material/Edit';
+import CircleIcon from '@mui/icons-material/Circle';
+import AccessTimeIcon from '@mui/icons-material/AccessTime';
+import LocationOnIcon from '@mui/icons-material/LocationOn';
 import { useState } from 'react';
 
-const ScheduleCard = () => {
+const ScheduleCard = (props) => {
+    // const address = 'Tehran, Iran University of Science and Technology, Computer Engineering Department';
+    // const arrivalTime = {hour: 7, minute: 0};
+    // const returnTime = {hour: 16, minute: 30};
+    // const capacity = 250;
+    // const days = {
+    //     'Sat': true,
+    //     'Sun': true,
+    //     'Mon': true,
+    //     'Teu': true,
+    //     'Wed': true,
+    //     'Thu': false,
+    //     'Fri': false
+    // };
+    // const details = '    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.';
+    // const {schedule} = props;
+    console.log(props)
+    const {address, arrivalTime, returnTime, capacity, days, details} = props.schedule;
+    
+    const formatTime = (time) => {
+        return time.hour.toLocaleString('en-US', {
+            minimumIntegerDigits: 2,
+            useGrouping: false
+        }) + ':' + time.minute.toLocaleString('en-US', {
+            minimumIntegerDigits: 2,
+            useGrouping: false
+        });
+    }
     return (
         <Card
             sx={{
-                p: 2
+                // flexGrow: 1,
+                // maxHeight: 'auto',
+                p: 2,
+                height: '100%'
             }}
         >
-            <Box width={800} height={192}/>
+            <Stack width={800} height='100%' spacing={2}>
+                <Stack direction='row' spacing={3} sx={{p: 0.5, alignItems: 'center'}}>
+                    <LocationOnIcon fontSize='large' />
+                    <Typography variant='h6' sx={{flexGrow: 1, maxWidth: 'auto'}}>
+                        {address}
+                    </Typography>
+                    <Stack spacing={0.5} sx={{alignItems: 'center'}}>
+                        <Stack direction='row' spacing={1} sx={{alignItems: 'center'}}>
+                            <AccessTimeIcon fontSize='small' />
+                            <Typography variant='body1' sx={{width: 'max-content'}}>
+                                {formatTime(arrivalTime)} - {formatTime(returnTime)}
+                            </Typography>
+                        </Stack>
+                        <Stack direction='row' spacing={0.5}>
+                            {['Sat', 'Sun', 'Mon', 'Teu', 'Wed', 'Thu', 'Fri'].map((day) => (
+                                <Tooltip title={day}>
+                                    <CircleIcon sx={{fontSize: '14px'}} color={days[day] ? 'primary' : 'disabled'} />
+                                </Tooltip>
+                            ))}
+                        </Stack>
+                    </Stack>
+                    <Typography variant='h6' sx={{flexShrink: 0, width: 120, textAlign: 'end'}}>
+                        {capacity === 0 ? 'No': capacity} Seats Left
+                    </Typography>
+                </Stack>
+                <Divider />
+                <Typography variant='body1' sx={{flexGrow: 1, maxHeight: 'auto'}}>
+                    {details}
+                </Typography>
+                <Divider />
+                <Stack direction='row' spacing={2} justifyContent='flex-end'>
+                    <Button variant='outlined' startIcon={<EditIcon />}>
+                        Edit
+                    </Button>
+                    <Button variant='contained'>
+                        Manage Seats
+                    </Button>
+                </Stack>
+            </Stack>
         </Card>
     )
 }
--- frontend404/src/views/Transportation/viewSchedules.js
+++ frontend404/src/views/Transportation/viewSchedules.js
@@ -1,30 +1,129 @@
 // design #1
 
-import { theme } from '../../theme';
-import Box from '@mui/material/Box';
-import Card from '@mui/material/Card';
-import Stack from '@mui/material/Stack';
-import Button from '@mui/material/Button';
-import Select from '@mui/material/Select';
-import Divider from '@mui/material/Divider';
-import MenuItem from '@mui/material/MenuItem';
-import Checkbox from '@mui/material/Checkbox';
-import Container from '@mui/material/Container';
-import TextField from '@mui/material/TextField';
-import InputLabel from '@mui/material/InputLabel';
-import Typography from '@mui/material/Typography';
-import Pagination from '@mui/material/Pagination';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import FormControl from '@mui/material/FormControl';
-import Search from '@mui/icons-material/Search';
-import { DashboardLayout } from '../../components/dashboard-layout';
-import { ThemeProvider } from '@mui/material/styles';
-import CssBaseline from '@mui/material/CssBaseline';
-import { useState } from 'react';
-import ScheduleCard from '../../components/transportation/scheduleCard';
-import mapPlaceholder from '../../images/map-placeholder-1.png';
+import { theme } from "../../theme";
+import Box from "@mui/material/Box";
+import Card from "@mui/material/Card";
+import Stack from "@mui/material/Stack";
+import Button from "@mui/material/Button";
+import Select from "@mui/material/Select";
+import Divider from "@mui/material/Divider";
+import MenuItem from "@mui/material/MenuItem";
+import Checkbox from "@mui/material/Checkbox";
+import Container from "@mui/material/Container";
+import TextField from "@mui/material/TextField";
+import InputLabel from "@mui/material/InputLabel";
+import Typography from "@mui/material/Typography";
+import Pagination from "@mui/material/Pagination";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import InputAdornment from "@mui/material/InputAdornment";
+import FormControl from "@mui/material/FormControl";
+import AddIcon from "@mui/icons-material/Add";
+import SearchIcon from "@mui/icons-material/Search";
+import { DashboardLayout } from "../../components/dashboard-layout";
+import { ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import { useState } from "react";
+import ScheduleCard from "../../components/transportation/scheduleCard";
+import mapPlaceholder from "../../images/map-placeholder-1.png";
 
-const ViewTransportationSchedulesView = () => {   
+const lorem = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.";
+
+const schedules = [
+    {
+        address: "Location 1",
+        arrivalTime: {hour: 7, minute: 0},
+        returnTime: {hour: 16, minute: 30},
+        capacity: 25,
+        capacityMax: 30,
+        days: {
+            "Sat": true,
+            "Sun": true,
+            "Mon": true,
+            "Teu": true,
+            "Wed": true,
+            "Thu": false,
+            "Fri": false
+        },
+        details: lorem,
+    },
+    {
+        address: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor",
+        arrivalTime: {hour: 7, minute: 0},
+        returnTime: {hour: 16, minute: 30},
+        capacity: 25,
+        capacityMax: 30,
+        days: {
+            "Sat": true,
+            "Sun": true,
+            "Mon": true,
+            "Teu": true,
+            "Wed": true,
+            "Thu": false,
+            "Fri": false
+        },
+        details: lorem,
+    },
+    {
+        address: "Location 3",
+        arrivalTime: {hour: 7, minute: 0},
+        returnTime: {hour: 16, minute: 30},
+        capacity: 25,
+        capacityMax: 30,
+        days: {
+            "Sat": true,
+            "Sun": true,
+            "Mon": true,
+            "Teu": true,
+            "Wed": true,
+            "Thu": false,
+            "Fri": false
+        },
+        details: lorem,
+    },
+    {
+        address: "Location 4",
+        arrivalTime: {hour: 7, minute: 0},
+        returnTime: {hour: 16, minute: 30},
+        capacity: 25,
+        capacityMax: 30,
+        days: {
+            "Sat": true,
+            "Sun": true,
+            "Mon": true,
+            "Teu": true,
+            "Wed": true,
+            "Thu": false,
+            "Fri": false
+        },
+        details: lorem,
+    },
+    {
+        address: "Location 5",
+        arrivalTime: {hour: 7, minute: 0},
+        returnTime: {hour: 16, minute: 30},
+        capacity: 25,
+        capacityMax: 30,
+        days: {
+            "Sat": true,
+            "Sun": true,
+            "Mon": true,
+            "Teu": true,
+            "Wed": true,
+            "Thu": false,
+            "Fri": false
+        },
+        details: lorem,
+    },
+]
+
+const ViewTransportationSchedulesView = () => {
+    const pageCount = Math.ceil(schedules.length / 2);
+    const [page, setPage] = useState(1);
+
+    const handlePageChange = (e, p) => {
+        setPage(p);
+    }
+
     return (
         <ThemeProvider theme={theme}>
             <CssBaseline />
@@ -36,53 +135,94 @@ const ViewTransportationSchedulesView = () => {
             >
                 <Container maxWidth={false} sx={{
                     p: 3,
-                    height: '100%',
+                    height: "100%",
                 }}>
-                    <Stack spacing={3} sx={{height: '100%', width: '100%'}}>
+                    <Stack spacing={3} sx={{height: "100%", width: "100%"}}>
                         <Box>
                             <Typography
                                 variant="h4"
                             >
-                                Add New Schedule
+                                View Schedules
                             </Typography>
                         </Box>
 
-                        <Stack spacing={4} direction='row'
+                        <Stack spacing={4} direction="row"
                             sx={{
                                 flexGrow: 1,
-                                height: '100%',
-                                alignItems: 'center'
+                                height: "100%",
+                                alignItems: "center"
                             }}
                         >
                             <Stack spacing={3} sx={{
-                                height: '100%',
-                                alignItems: 'center'
+                                height: "100%",
+                                alignItems: "center"
                             }}>
+                                <Card sx={{width: "100%", p: 2}}>
+                                    <Stack direction="row" spacing={2}>
+                                        <TextField
+                                        sx={{
+                                            flexGrow: 1,
+                                            maxWidth: "auto"
+                                        }}
+                                        InputProps={{
+                                            startAdornment: (
+                                            <InputAdornment position="start">
+                                                <SearchIcon />
+                                            </InputAdornment>
+                                            )
+                                        }}
+                                        placeholder="Search address"
+                                        variant="outlined"
+                                        />
+                                        <Button variant="contained" startIcon={<AddIcon />}>
+                                            Add Schedule
+                                        </Button>
+                                    </Stack>
+                                </Card>
                                 <Stack spacing={2} sx={{
-                                    width: 'fit-content',
+                                    width: "fit-content",
                                     flexGrow: 1,
-                                    maxHeight: 'auto'
+                                    maxHeight: "auto"
                                 }}>
-                                    <ScheduleCard />
-                                    <ScheduleCard />
-                                    <ScheduleCard />
+                                    <Box sx={{
+                                        flexGrow: 1,
+                                        flexBasis: 0,
+                                        maxHeight: "auto",
+                                    }}>
+                                        <ScheduleCard schedule={schedules[page * 2 - 2]}/>
+                                    </Box>
+                                    <Box sx={{
+                                        flexGrow: 1,
+                                        flexBasis: 0,
+                                        maxHeight: "auto",
+                                    }}>
+                                        {
+                                            page * 2 <= schedules.length
+                                            && <ScheduleCard schedule={schedules[page * 2 - 1]}/>
+                                        }
+                                    </Box>
                                 </Stack>
-                                <Pagination count={10} color='primary'/>
+                                <Pagination
+                                    count={pageCount}
+                                    color="primary"
+                                    page={page}
+                                    onChange={handlePageChange}
+                                />
                             </Stack>
                             <Box sx={{
-                                // width: '100%',
+                                // width: "100%",
                                 // width: 600,
                                 flexGrow: 1,
-                                maxWidth: 'auto',
+                                maxWidth: "auto",
                                 // height: 320,
-                                // height: '100%',
-                                bgcolor: '#dddddd',
-                                // position: 'sticky',
+                                // height: "100%",
+                                bgcolor: "#dddddd",
+                                // position: "sticky",
                                 // top: 88,
                                 zIndex: -1
                             }}>
                                 <img
-                                    styles={{width: '100%', height: '100%'}}
+                                    styles={{width: "100%", height: "100%"}}
                                     src={mapPlaceholder} 
                                 />
                             </Box>
