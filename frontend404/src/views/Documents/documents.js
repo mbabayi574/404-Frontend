f73b1c99f8d09e55b0e0da57b640a2ea615021fc
Diyar Hamedi <diyarhamedi81@gmail.com>
2022-06-11 00:04:04 +0430

    Add pagination to documents page

--- frontend404/src/views/Documents/documents.js
+++ frontend404/src/views/Documents/documents.js
@@ -6,7 +6,7 @@ import Button from "@mui/material/Button";
 import Divider from '@mui/material/Divider';
 import Container from "@mui/material/Container";
 import Typography from "@mui/material/Typography";
-// import Typography from "@mui/material/Typography";
+import Pagination from "@mui/material/Pagination";
 import AddIcon from "@mui/icons-material/Add";
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
@@ -15,13 +15,20 @@ import useAPI from "useAPI";
 import AddDocument from "./components/addDocument";
 
 const Documents = () => {
+  const [page, setPage] = useState(1);
   const [documents, setDocuments] = useState([]);
   const navigate = useNavigate();
   const api = useAPI();
+  const documentsPerPage = 4;
+  const pageCount = Math.ceil(documents.length / documentsPerPage);
 
   useEffect(() => {
     getDocuments();
   }, []);
+
+  useEffect(() => {
+    setPage(Math.max(Math.min(page, pageCount), 1));
+  }, [page, pageCount])
 
   const getDocuments = () => {
     var config = {
@@ -40,6 +47,39 @@ const Documents = () => {
       });
   }
 
+  const DocumentsView = () => {
+    const paginationStart = documentsPerPage * (page - 1);
+    const items = new Array(documentsPerPage).fill()
+      .map((_, index) => documents[paginationStart + index]);
+    console.log(items);
+    return (
+      <Stack spacing={2} sx={{
+        width: "100%",
+        height: "100%",
+        alignItems: "center",
+      }}
+      >
+        <Grid container spacing={2}>
+          {items.map((document) => (
+            <Grid item xs={12} md={6}>
+              <Box height="35vh">
+                {document
+                  ? <DocumentItem document={document} reload={getDocuments} />
+                  : <Box width="100%" height="100%" />
+                }
+              </Box>
+            </Grid>
+          ))}
+        </Grid>
+        <Pagination
+          count={pageCount}
+          page={page} onChange={(event, value) => setPage(value)}
+        />
+      </Stack>
+    );
+  }
+
+
   return (
     <Box
       component="main"
@@ -54,9 +94,9 @@ const Documents = () => {
           height: "90vh",
         }}
       >
-        <Stack spacing={1} direction="row" sx={{ p: 1, height: "100%", width: "100%" }}>
+        <Stack spacing={2} direction="row" sx={{ p: 1, height: "100%", width: "100%" }}>
           <Box flexGrow={1} maxWidth="auto">
-            <AddDocument reload={getDocuments}/>
+            <AddDocument reload={getDocuments} />
           </Box>
           <Box sx={{
             height: "100%",
@@ -75,18 +115,7 @@ const Documents = () => {
                 Document you add will appear here
               </Typography>
               :
-              <Grid container spacing={2} sx={{
-                height: "100%",
-                overflowY: "auto"
-              }}>
-                {documents.map((document) => (
-                  <Grid item xs={12} md={6}>
-                    <Box height="35vh">
-                      <DocumentItem document={document} reload={getDocuments} />
-                    </Box>
-                  </Grid>
-                ))}
-              </Grid>
+              <DocumentsView />
             }
           </Box>
         </Stack>
