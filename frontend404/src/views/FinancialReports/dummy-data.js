c9c20515fea8e5df6d852c2e2e71f1c596239a89
Diyar Hamedi <diyarhamedi81@gmail.com>
2022-07-03 11:18:48 +0430

    Add financial report data chart

--- frontend404/src/views/FinancialReports/dummy-data.js
+++ frontend404/src/views/FinancialReports/dummy-data.js
@@ -1,4 +1,5 @@
 import { faker } from "@faker-js/faker";
+import { useState } from "react";
 
 export const dummyEvents = [];
 
@@ -26,6 +27,51 @@ export const createRandomEvent = () => {
   };
 };
 
-Array.from({length: 10}).forEach(() => {
+Array.from({ length: 5 }).forEach(() => {
   dummyEvents.push(createRandomEvent());
 });
+
+export const useReportData = () => {
+  const [reportData, setReportData] = useState([]);
+  const getReportData = (startDate, endDate, precision) => {
+    let tempData = [];
+    let totalRevenue = 0;
+    const step = getDateStep(precision);
+    for (let date = startDate; date.valueOf() < endDate.valueOf(); date = increaseDate(date, precision)) {
+      const item = addRandomDataItem(date, totalRevenue);
+      tempData.push(item);
+      totalRevenue = item.total_revenue;
+    }
+    setReportData(tempData);
+  };
+  const increaseDate = (date, precision) => {
+    const step = getDateStep(precision);
+    date.setDate(date.getDate() + step);
+    return date;
+  }
+  const addRandomDataItem = (date, totalRevenue) => {
+    const currentRevenue = faker.datatype.float({ min: -1000, max: 1000 });
+    return {
+      id: faker.random.numeric(10),
+      date: date.toLocaleDateString(),
+      revenue: currentRevenue,
+      total_revenue: parseFloat((totalRevenue + currentRevenue).toFixed(2)),
+    };
+  };
+  const getDateStep = (precision) => {
+    switch (precision) {
+      case 'daily':
+        return 1;
+      case 'weekly':
+        return 7;
+      case 'monthly':
+        return 30;
+      case 'annual':
+        return 365;
+    }
+  };
+  return {
+    reportData,
+    getReportData
+  };
+}
