a6afbf7beb6ea9333d29a9113bccafd06cbbed2e
Diyar Hamedi <diyarhamedi81@gmail.com>
2022-07-05 20:44:19 +0430

    Customize financial statistics chart style

--- frontend404/src/views/FinancialReports/components/financialStatisticsChart.js
+++ frontend404/src/views/FinancialReports/components/financialStatisticsChart.js
@@ -7,11 +7,29 @@ import {
   CartesianGrid,
   Tooltip,
   Legend,
-  ResponsiveContainer
+  ResponsiveContainer,
+  Cell
 } from "recharts";
 
 const FinancialStatisticsChart = (props) => {
+  const gainColor = '#14B8A6';
+  const gainColorArea = '#43C6B7';
+  const lossColor = '#D14343';
+  const lossColorArea = '#DA6868';
+
   const { statistics } = props;
+  const maxTotalRevenue = Math.max(...statistics.map(item => item.total_revenue));
+  const minTotalRevenue = Math.min(...statistics.map(item => item.total_revenue));
+  const totalRevenueRange = maxTotalRevenue - minTotalRevenue;
+  const percentage = (maxTotalRevenue / totalRevenueRange) * 100;
+  const areaChartGradient = (
+    <linearGradient id="gradient" x1="0" y1="0" x2="0%" y2="100%">
+      <stop offset="0%" stopColor={gainColorArea} />
+      <stop offset={`${percentage}%`} stopColor={gainColorArea} />
+      <stop offset={`${percentage}%`} stopColor={lossColorArea} />
+      <stop offset="100%" stopColor={lossColorArea} />
+    </linearGradient>
+  );
 
   return (
     <ResponsiveContainer width="75%" height="100%">
@@ -19,6 +37,7 @@ const FinancialStatisticsChart = (props) => {
         width={500}
         height={400}
         data={statistics}
+        baseValue="0"
         margin={{
           top: 20,
           right: 20,
@@ -26,13 +45,28 @@ const FinancialStatisticsChart = (props) => {
           left: 20,
         }}
       >
+        <defs>
+          {areaChartGradient}
+        </defs>
         <CartesianGrid stroke="#f5f5f5" />
         <XAxis dataKey="date" scale="band" />
         <YAxis />
         <Tooltip />
         <Legend />
-        <Area type="monotone" dataKey="total_revenue" fill="#8884d8" stroke="#8884d8" />
-        <Bar dataKey="revenue" barSize={20} fill="#413ea0" />
+
+        <Bar name="Revenue" dataKey="revenue" barSize={10}>
+          {statistics.map(item => (
+            <Cell
+            fill={(item.revenue >= 0) ? gainColor : lossColor}
+            />
+          ))}
+        </Bar>
+        <Area
+          name="Total Revenue"
+          type="monotone"
+          dataKey="total_revenue"
+          fill="url(#gradient)"
+        />
       </ComposedChart>
     </ResponsiveContainer>
   );
